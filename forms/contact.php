495ca9bc02fea4936cb2bf404cd84da8eb430e60
giselle <giselle@devusol.com>
2022-11-30 13:18:16 -0500

    instagram and pinterest switch

--- forms/contact.php
+++ forms/contact.php
@@ -1,25 +1,25 @@
 <?php
 
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'giselle.a.borrero@gmail.com';
+// Replace contact@example.com with your real receiving email address
+$receiving_email_address = 'info@ranjaniGroth.com';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
+  include($php_email_form);
+} else {
+  die('Unable to load the "PHP Email Form" Library!');
+}
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+$contact = new PHP_Email_Form;
+$contact->ajax = true;
 
-  // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
-  /*
+$contact->to = $receiving_email_address;
+$contact->from_name = $_POST['name'];
+$contact->from_email = $_POST['email'];
+$contact->subject = $_POST['subject'];
+
+// Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials
+/*
   $contact->smtp = array(
     'host' => 'example.com',
     'username' => 'example',
@@ -28,8 +28,8 @@
   );
   */
 
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+$contact->add_message($_POST['name'], 'From');
+$contact->add_message($_POST['email'], 'Email');
+$contact->add_message($_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+echo $contact->send();
